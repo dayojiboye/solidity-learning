7863e3ecf35eceb275c4909017296ac9ce392bfc
dayojiboye <dayojiboye2@gmail.com>
2025-01-09 11:12:00 +0100

    basic crypto token

---
+++ subcurrency.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2 <0.9.0;
+
+contract Coin {
+    address public minter;
+    mapping(address => uint256) public balances;
+
+    event Sent(address from, address to, uint256 amount);
+
+    error insufficientBalance(
+        uint256 amountRequested,
+        uint256 amountAavailable
+    );
+
+    constructor() {
+        minter = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender == minter) {
+            _;
+        }
+    }
+
+    function mint(address receiver, uint256 amount) public onlyOwner {
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint256 amount) public {
+        if (amount > balances[msg.sender]) {
+            revert insufficientBalance({
+                amountRequested: amount,
+                amountAavailable: balances[msg.sender]
+            });
+        }
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+    }
+}
